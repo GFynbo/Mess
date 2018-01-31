e0ad3d31b2160f2502e665c07b685d7310b07e1c
Gavin Fynbo <gavfynbo@gmail.com>
2018-01-31 18:06:06 -0500

    up and running

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Messing Around' });
+});
+
+/* GET secret page. */
+router.get('/secret', function(req, res, next) {
+  res.render('secret', { title: 'Login' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Logging in...' });
 });
 
 module.exports = router;
